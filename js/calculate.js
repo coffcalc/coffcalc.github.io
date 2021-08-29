d24ca067d06924cd7c85565964255b3db405edca
mikolajbendyk <mikolaj.bendyk@gmail.com>
2021-08-29 15:13:55 +0200

    x

--- js/calculate.js
+++ js/calculate.js
@@ -5,18 +5,18 @@ const strength = document.getElementById("strength");
 const coffee = document.getElementById("coffee");
 const water = document.getElementById("water");
 
-brew.addEventListener('input', calculate())
-strength.addEventListener('input', calculate())
+brew.addEventListener('input', calculate)
+strength.addEventListener('input', calculate)
 
 
-const calculate = () => {
+function calculate() {
     const cof = (brew / strength) - absorption.average;
     coffee.innerText = cof;
     water.innerText = brew + absorption.average * cof;
 }
 
-const title = document.getElementsByTagName('h1')[0];
+var title = document.getElementsByTagName('h1')[0];
 
-title.addEventListener('click', e => {
+title.addEventListener('click', () => {
     title.style.color = "red";
 })
