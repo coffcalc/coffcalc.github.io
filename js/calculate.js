38b7c779068eda0e56f281d4d18dd8a0fa9348a4
mikolajbendyk <mikolaj.bendyk@gmail.com>
2021-08-29 16:17:43 +0200

    x

--- js/calculate.js
+++ js/calculate.js
@@ -1,9 +1,9 @@
 import { absorption } from "./constans.js";
 
 const brew = document.getElementById("brew");
-const brewVal = parseInt(brew.value);
+let brewVal = parseInt(brew.value);
 const strength = document.getElementById("strength");
-const strengthVal = parseInt(strength.value);
+let strengthVal = parseInt(strength.value);
 const coffee = document.getElementById("coffee");
 const water = document.getElementById("water");
 
@@ -12,6 +12,8 @@ strength.addEventListener('change', calculate);
 
 
 function calculate() {
+    brewVal = brew.value;
+    strengthVal = strength.value;
     const cof = (brewVal / strengthVal) - absorption.average;
     coffee.innerText = cof;
     water.innerText = brewVal + absorption.average * cof;
