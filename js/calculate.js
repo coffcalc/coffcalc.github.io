482cdd095b6df6b07ca5f6116e0f37c0a36343e4
mikolajbendyk <mikolaj.bendyk@gmail.com>
2021-10-30 20:47:08 +0200

    added calculation for certain ammount of coffee

--- js/calculate.js
+++ js/calculate.js
@@ -1,17 +1,17 @@
 import { absorption } from "./constans.js";
 
-const brew = document.getElementById("brew");
+const brew = document.getElementById("brew-input");
 let brewVal = parseInt(brew.value);
-const strength = document.getElementById("strength");
+const strength = document.getElementById("strength-input");
 let strengthVal = parseInt(strength.value);
 const coffee = document.getElementById("coffee");
 const water = document.getElementById("water");
 
-brew.addEventListener('change', calculate);
-strength.addEventListener('change', calculate);
+brew.addEventListener('change', calculateBrew);
+strength.addEventListener('change', calculateBrew);
 
 
-function calculate() {
+function calculateBrew() {
     brewVal = parseInt(brew.value);
     strengthVal = parseInt(strength.value);
     const cof = brewVal / (strengthVal - absorption.average);
@@ -26,13 +26,38 @@ const brewBtn = document.getElementById("brew-btn");
 const coffeeBtn = document.getElementById("coffee-btn");
 const brewInputCon = document.getElementById("brew-input-con");
 const coffeeInputCon = document.getElementById("coffee-input-con");
+const coffeeResultCon = document.getElementById("coffee-result-con");
+const brewResultCon = document.getElementById("brew-result-con");
+const coffeeInput = document.getElementById("coffee-input");
+const brewResult = document.getElementById("brew");
 
 brewBtn.addEventListener('click', switchFields);
 coffeeBtn.addEventListener('click', switchFields);
+coffeeInput.addEventListener('change', calculateCoffee);
+strength.addEventListener('change', calculateCoffee);
 
 function switchFields() {
     brewBtn.classList.toggle("clicked");
     coffeeBtn.classList.toggle("clicked");
     brewInputCon.classList.toggle("hidden");
     coffeeInputCon.classList.toggle("hidden");
+    coffeeResultCon.classList.toggle("hidden")
+    brewResultCon.classList.toggle("hidden");
+    brew.value = "";
+    strength.value = "";
+    coffeeInput.value = "";
+    coffee.innerText = 0;
+    water.innerText = 0;
+    brewResult.innerText = 0;
 };
+
+function calculateCoffee() {
+    const coffeeVal = parseInt(coffeeInput.value);
+    const strengthVal = parseInt(strength.value);
+    const wat = coffeeVal * strengthVal;
+    const brw = wat - absorption.average * coffeeVal;
+    if (!isNaN(wat) && !isNaN(brw) && brw > 0) {
+        water.innerText = Math.round((wat + Number.EPSILON) * 10) / 10;
+        brewResult.innerText = Math.round((brw + Number.EPSILON) * 10) / 10;
+    }
+}
